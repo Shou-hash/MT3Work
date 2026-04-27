84727088c26bfa575ea251246f913765a436aa59
Shou <k025g0060@g.neec.ac.jp>
2026-04-27 17:56:30 +0900

    MT3_00_02

--- main.cpp
+++ main.cpp
@@ -9,6 +9,96 @@ struct Vector2
 	float z;
 };
 
+struct Matrix4x4
+{
+	float m[4][4];
+};
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result.m[i][j] = m1.m[i][j] + m2.m[i][j];
+		}
+	}
+	return result;
+}
+
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result.m[i][j] = m1.m[i][j] - m2.m[i][j];
+		}
+	}
+	return result;
+}
+
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			result.m[i][j] = m1.m[i][0] * m2.m[0][j] + m1.m[i][1] * m2.m[1][j] + m1.m[i][2] * m2.m[2][j] + m1.m[i][3] * m2.m[3][j];
+		}
+	}
+	return result;
+}
+
+Matrix4x4 Inverse(const Matrix4x4&)
+{
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i)
+	{
+		result.m[i][i] = 1.0f;
+	}
+	return result;
+}
+
+Matrix4x4 Transpose(const Matrix4x4&)
+{
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i)
+	{
+		result.m[i][i] = 1.0f;
+	}
+	return result;
+}
+
+Matrix4x4 MakeIdentity4x4()
+{
+	Matrix4x4 result{};
+	for (int i = 0; i < 4; ++i)
+	{
+		result.m[i][i] = 1.0f;
+	}
+	return result;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label)
+{
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			// 行列の表示はラベルの分(1行分)下にずらすため (i + 1) * kRowHeight とする
+			Novice::ScreenPrintf(x + j * kColumnWidth, y + (i + 1) * kRowHeight, "%6.2f", matrix.m[i][j]);
+		}
+	}
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
@@ -18,6 +108,20 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Matrix4x4 m1 = {
+		3.2f,0.7f,9.6f,4.4f,
+		5.5f,1.3f,7.8f,2.1f,
+		6.4f,8.0f,2.6f,1.0f,
+		0.5f,7.2f,5.1f,3.3f
+	};
+
+	Matrix4x4 m2 = {
+		1.2f,4.5f,6.7f,8.9f,
+		2.3f,5.6f,7.8f,9.0f,
+		3.4f,6.7f,8.9f,1.2f,
+		4.5f,7.8f,9.0f,2.3f
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -32,6 +136,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 resultAdd = Add(m1, m2);
+		Matrix4x4 resultMultiply = Multiply(m1, m2);
+		Matrix4x4 resultSubtract = Subtract(m1, m2);
+		Matrix4x4 InverseM1 = Inverse(m1);
+		Matrix4x4 InverseM2 = Inverse(m2);
+		Matrix4x4 TransposeM1 = Transpose(m1);
+		Matrix4x4 TransposeM2 = Transpose(m2);
+		Matrix4x4 Identity = MakeIdentity4x4();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -39,6 +152,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, resultAdd, "Add");
+		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply, "Multiply");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, InverseM1, "InverseM1");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 4, InverseM2, "InverseM2");
+		MatrixScreenPrintf(kColumnWidth * 5, 0, TransposeM1, "TransposeM1");
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, TransposeM2, "TransposeM2");
+		MatrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5 * 2, Identity, "Identity");
 
 		///
 		/// ↑描画処理ここまで
